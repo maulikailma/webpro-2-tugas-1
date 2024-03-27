292c106643820715724be16e610657c685a6f0d5
unknown <ilmamaulika@gmail.com>
2024-03-27 10:32:02 +0700

    webprotugas2

---
+++ webprotugas1.js
@@ -0,0 +1,6 @@
+ TODO: change myArray to contain the variables
+var myArray = ["Ilma Maulika Islami Ranti", 42, true , "Putri Utami"];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
